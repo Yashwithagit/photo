44e108c0d9a9e124353179775efa6213e82702e5
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-07-23 21:00:06 +0530

    FEAT: api path added

--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -1,12 +1,35 @@
 'use client';
-import React from 'react'
+import React, { useState } from 'react'
 import { CardContainer, CardHeader, FormContainer, FormField, FormFieldIcon, FormFieldInput, LinkContainer, LinkText, PhotoGraphyBgContainer } from '@/styles/globalStyles'
 import Link from 'next/link'
 import * as MdIcons from 'react-icons/md'
 import * as RiIcons from 'react-icons/ri'
 
+interface FormDataProps {
+  email?: string;
+  password?: string;
+  contact?: number;
+  username?: string;
 
-const SignUp = () => {
+
+}
+const SignUp: React.FC<FormDataProps> = () => {
+  const [formData, setFormData] = useState<FormDataProps>({
+    username: '',
+    email: '',
+    password: ''
+
+  })
+
+  // handle Sign Up form submit
+  const onSignUpFormSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+   try {
+    
+   } catch (error) {
+    
+   }
+  }
   return (
     <><PhotoGraphyBgContainer />
       <CardContainer>
@@ -14,23 +37,23 @@ const SignUp = () => {
         <FormContainer action="#">
           <FormField>
             <FormFieldIcon><MdIcons.MdEmail /></FormFieldIcon>
-            <FormFieldInput type="text" required placeholder="Email" />
+            <FormFieldInput name={formData.email} value={formData.email} type="text" required placeholder="Email" onChange={(e) => setFormData({ ...formData, email: String(e.target.value) })} />
           </FormField>
           <FormField>
             <FormFieldIcon><RiIcons.RiUser3Fill /></FormFieldIcon>
-            <FormFieldInput type="text" required placeholder="User Name" />
+            <FormFieldInput name={formData.username} value={formData.username} type="text" required placeholder="User Name" onChange={(e) => setFormData({ ...formData, username: String(e.target.value) })} />
           </FormField>
           <FormField>
             <FormFieldIcon><MdIcons.MdPhone /></FormFieldIcon>
-            <FormFieldInput type="text" required placeholder="Contact" />
+            <FormFieldInput name={String(formData.contact)} value={String(formData.contact)} type="number" required placeholder="Contact" onChange={(e) => setFormData({ ...formData, contact: parseInt(e.target.value) })} />
           </FormField>
           <FormField>
             <FormFieldIcon><RiIcons.RiLockPasswordFill /></FormFieldIcon>
-            <FormFieldInput type="password" required placeholder="Password" />
+            <FormFieldInput name={formData.password} value={formData.password} type="password" required placeholder="Password" onChange={(e) => setFormData({ ...formData, password: String(e.target.value) })} />
           </FormField>
 
           <FormField>
-            <FormFieldInput type="submit" value="SIGN UP" />
+            <FormFieldInput type="submit" value="SIGN UP" onClick={(e: any) => onSignUpFormSubmit(e)} />
           </FormField>
         </FormContainer>
 
@@ -43,7 +66,7 @@ const SignUp = () => {
   )
 }
 
-export default SignUp
+export default React.memo(SignUp)
 
 
 
