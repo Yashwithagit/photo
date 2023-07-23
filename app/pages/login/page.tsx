b95a88c4fb7505071ff0c364c503652d8cd14b79
yashwitha-at-411977029542 <D/b20m+IOckszuzuPuS4+r+Yorqu2R1a8ymbIA6yjhI=>
2023-07-23 14:45:35 +0530

    FEAT: login page

---
+++ app/pages/login/page.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { CardContainer, CardHeader, FormContainer, FormField, FormFieldIcon, FormFieldInput, LinkContainer, LinkText, PhotoGraphyBgContainer } from '@/styles/globalStyles'
+import Link from 'next/link'
+import * as MdIcons from 'react-icons/md'
+import * as RiIcons from 'react-icons/ri'
+
+
+const Login = () => {
+  return (
+    <><PhotoGraphyBgContainer />
+      <CardContainer>
+        <CardHeader>Login</CardHeader>
+        <FormContainer action="#">
+          <FormField>
+            <FormFieldIcon><MdIcons.MdEmail /></FormFieldIcon>
+            <FormFieldInput type="text" required placeholder="Email" />
+          </FormField>
+          <FormField>
+            <FormFieldIcon><RiIcons.RiLockPasswordFill /></FormFieldIcon>
+            <FormFieldInput type="password" required placeholder="Password" />
+          </FormField>
+
+          <FormField>
+            <FormFieldInput type="submit" value="LOGIN" />
+          </FormField>
+        </FormContainer>
+
+        <LinkContainer>
+          Don&#x27;t have account?
+          <Link href={'/signUp'}><LinkText>Signup </LinkText></Link>
+        </LinkContainer>
+      </CardContainer ></>
+
+  )
+}
+
+export default Login
+
+
+
+
+
